124701926d174ff2635eabc36bf5ed1900ea298b
willbjones <will.b.jonesy@googlemail.com>
2019-11-19 10:07:29 +0000

    End of sec 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -11,9 +11,21 @@ class EventForm extends Component {
     host: ''
   }
   
+  componentDidMount() {
+    if(this.props.selectedEvent !== null) {
+      this.setState ({
+        ...this.props.selectedEvent
+      })
+    }
+  }
+
   handleFormSubmit = (evt) => {
     evt.preventDefault();
-    this.props.createEvent(this.state);
+    if(this.state.id) {
+      this.props.updateEvent(this.state);
+    } else {
+      this.props.createEvent(this.state);
+    }
   }
   
   handleInputChange = ({target: {name, value}}) => {
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -4,7 +4,7 @@ import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
+    const {event, selectEvent, deleteEvent} = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -13,29 +13,39 @@ class EventListItem extends Component {
               <Item.Image size='tiny' circular src={event.hostPhotoURL} />
               <Item.Content>
                 <Item.Header>{event.title}</Item.Header>
-                <Item.Description>
-                  Hosted by {event.hostedBy}
-                </Item.Description>
+                <Item.Description>Hosted by {event.hostedBy}</Item.Description>
               </Item.Content>
             </Item>
           </Item.Group>
         </Segment>
-        <Segment> 
+        <Segment>
           <span>
-            <Icon name='clock' /> {event.date} | 
+            <Icon name='clock' /> {event.date} |
             <Icon name='marker' /> {event.venue}
           </span>
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attendees && event.attendees.map(attendee => (
-              <EventListAttendee key={attendee.id} attendee={attendee} />
-            ))}
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
-          <Button as='a' color='teal' floated='right' content='View' />
+          <Button
+            onClick={() => deleteEvent(event.id)}
+            as='a'
+            color='red'
+            floated='right'
+            content='Delete' />
+          <Button
+            onClick={() => selectEvent(event)}
+            as='a'
+            color='teal'
+            floated='right'
+            content='View' />
         </Segment>
       </Segment.Group>
     );
