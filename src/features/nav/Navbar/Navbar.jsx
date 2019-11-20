9c239cb201793af347d73a5736895eeffed31195
willbjones <will.b.jonesy@googlemail.com>
2019-11-20 10:59:22 +0000

    End of sec 7

--- src/features/nav/Navbar/Navbar.jsx
+++ src/features/nav/Navbar/Navbar.jsx
@@ -26,6 +26,7 @@ class Navbar extends Component {
           </Menu.Item>
           <Menu.Item as={NavLink} to='/events' name='Events' />
           <Menu.Item as={NavLink} to='/people' name='People' />
+          <Menu.Item as={NavLink} to='/test' name='Test' />
           <Menu.Item>
             <Button
               as={Link}
